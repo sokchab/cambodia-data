ce02f8a13eb33d93b2bb226fa3cde3b12ddb4810
Jecobe74 <sokchab_pho143@live.com>
2022-09-20 14:17:39 +0700

    Init

---
+++ data/ProvinceData.php
@@ -0,0 +1,41 @@
+<?php
+namespace Database\Seeders\Data;
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.0
+ */
+
+/**
+ * Database `liveravel`
+ */
+
+/* `liveravel`.`zone__provinces` */
+class ProvinceData {
+  public static $data = array(
+    array('id' => '1','code' => '01','name' => 'Banteay Meanchey','kh_name' => 'បន្ទាយមានជ័យ','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '2','code' => '02','name' => 'Battambang','kh_name' => 'បាត់ដំបង','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '3','code' => '03','name' => 'Kampong Cham','kh_name' => 'កំពង់ចាម','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '4','code' => '04','name' => 'Kampong Chhnang','kh_name' => 'កំពង់ឆ្នាំង','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '5','code' => '05','name' => 'Kampong Speu','kh_name' => 'កំពង់ស្ពឺ','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '6','code' => '06','name' => 'Kampong Thom','kh_name' => 'កំពង់ធំ','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '7','code' => '07','name' => 'Kampot','kh_name' => 'កំពត','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '8','code' => '08','name' => 'Kandal','kh_name' => 'កណ្ដាល','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '9','code' => '09','name' => 'Koh Kong','kh_name' => 'កោះកុង','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '10','code' => '10','name' => 'Kratié','kh_name' => 'ក្រចេះ','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '11','code' => '11','name' => 'Mondul Kiri','kh_name' => 'មណ្ឌលគិរី','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '12','code' => '12','name' => 'Phnom Penh','kh_name' => 'ភ្នំពេញ','modify_date' => '02-02-2022','status' => '1','prefix' => 'រាធានី','deleted_at' => NULL),
+    array('id' => '13','code' => '13','name' => 'Preah Vihear','kh_name' => 'ព្រះវិហារ','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '14','code' => '14','name' => 'Prey Veng','kh_name' => 'ព្រៃវែង','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '15','code' => '15','name' => 'Pursat','kh_name' => 'ពោធិ៍សាត់','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '16','code' => '16','name' => 'Ratanak Kiri','kh_name' => 'រតនគិរី','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '17','code' => '17','name' => 'Siem Reap','kh_name' => 'សៀមរាប','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '18','code' => '18','name' => 'Preah Sihanouk','kh_name' => 'ព្រះសីហនុ','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '19','code' => '19','name' => 'Stung Treng','kh_name' => 'ស្ទឹងត្រែង','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '20','code' => '20','name' => 'Svay Rieng','kh_name' => 'ស្វាយរៀង','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '21','code' => '21','name' => 'Takeo','kh_name' => 'តាកែវ','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '22','code' => '22','name' => 'Otdar Meanchey','kh_name' => 'ឧត្ដរមានជ័យ','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '23','code' => '23','name' => 'Kep','kh_name' => 'កែប','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '24','code' => '24','name' => 'Pailin','kh_name' => 'ប៉ៃលិន','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL),
+    array('id' => '25','code' => '25','name' => 'Tboung Khmum','kh_name' => 'ត្បូងឃ្មុំ','modify_date' => '02-02-2022','status' => '1','prefix' => 'ខេត្ត','deleted_at' => NULL)
+  );
+}
